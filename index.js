115b722f9bc962fe554765c2e2bc926c25faff06
Zane Bjornerud <zbjornerud@Zanes-MacBook-Pro.local>
2024-09-09 11:50:20 -0600

    Added Persistence to Genres API

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 //be able to create a new genre
 //update an existing genre
 //delete an existing genre
-const Joi = require('joi');
+const mongoose = require('mongoose');
+
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB... '))
+    .catch(err => console.error('Could not connect to MongoDB... '));
 
 app.use(express.json());
 app.use('/api/genres', genres);
--- routes/genres.js
+++ routes/genres.js
@@ -1,11 +1,67 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const genres = [
-    {id: 1, name: 'Action'},
-    {id: 2, name: 'Horror'},
-    {id: 3, name: 'Romance'},
-]
+
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength: 50
+    }
+}));
+
+//getting all the genres
+router.get('/', async (req, res) => {
+    const genres = await Genre.find().sort('name');
+    res.send(genres);
+}); 
+
+//create a new genre
+router.post('/', async (req, res) => {
+    const { error } = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    let genre = new Genre({ name: req.body.name });
+    genre = await genre.save();
+
+    res.send(genre);
+});
+
+//update an existing genre
+router.put('/:id', async (req, res) => {
+    const { error } = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true
+    })
+
+    if(!genre) return res.status(404).send('The Genre with the given ID was not found');
+
+    res.send(genre);
+});
+
+//delete an existing genre
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndDelete(req.params.id);
+
+    if(!genre) return res.status(404).send('The Genre with the given ID was not found');
+
+    res.send(genre);
+});
+
+//get a specific genre
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.findById(req.params.id);
+
+    if(!genre) return res.status(404).send('The Genre with the given ID was not found');
+
+    res.send(genre);
+});
 
 function validateGenre(genre) {
     const schema = Joi.object({
@@ -14,53 +70,5 @@ function validateGenre(genre) {
 
     return schema.validate(genre);
 }
-
-//getting all the genres
-router.get('/', (req, res) => {
-    res.send(genres);
-}); 
-
-//create a new genre
-router.post('/', (req, res) => {
-    const { error } = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-    genres.push(genre);
-    res.send(genre);
-});
-
-//update an existing genre
-router.put('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The Genre with the given ID was not found');
-
-    const { error } = validateGenre(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    genre.name = req.body.name;
-    res.send(genre);
-});
-
-//delete an existing genre
-router.delete('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The Genre with the given ID was not found');
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-
-    res.send(genre);
-})
-
-//get a specific genre
-router.get('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if(!genre) return res.status(404).send('The Genre with the given ID was not found');
-    res.send(genre);
-})
 
 module.exports = router;
