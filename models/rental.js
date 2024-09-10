0714389b81f8376d07440c49ec66b06bf818029d
Zane Bjornerud <zbjornerud@Zanes-MacBook-Pro.local>
2024-09-10 12:57:30 -0600

    added joi objectId validation

--- models/rental.js
+++ models/rental.js
@@ -57,8 +57,8 @@ const Rental = mongoose.model('Rental', new mongoose.Schema({
 
 function validateRental(rental) {
   const schema = Joi.object({
-    customerId: Joi.string().required(),
-    movieId: Joi.string().required()
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
   });
 
   return schema.validate(rental);
