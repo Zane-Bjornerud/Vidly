596be09792fe72ac99c0ae95685e3c16b22180ac
Zane Bjornerud <zbjornerud@Zanes-MacBook-Pro.local>
2024-09-24 16:24:04 -0600

    replaced bcrypt w bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 
 const Joi = require('joi');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const _ = require('lodash');
 const {User} = require('../models/user');
 const mongoose = require('mongoose');
